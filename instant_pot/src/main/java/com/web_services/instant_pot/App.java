40367fbda123aedd8be7b378c73e10ebb4eaf54e
Jonathan <warkentin.jon@gmail.com>
2019-09-26 16:06:56 -0500

    create Maven project

---
+++ instant_pot/src/main/java/com/web_services/instant_pot/App.java
@@ -0,0 +1,9 @@
+package com.web_services.instant_pot;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ instant_pot/src/main/java/com/web_services/instant_pot/model/Product.java
@@ -0,0 +1,38 @@
+package com.web_services.instant_pot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String name;
+
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
