f91f3f8652b8812b71b286567be4d30ed264d2ae
Jonathan <warkentin.jon@gmail.com>
2019-10-13 12:27:03 -0500

    start to fill in Address and Payment classes, add them to hibernate config

--- instant_pot/src/main/java/com/web_services/instant_pot/domain/Address.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/domain/Address.java
@@ -1,5 +1,21 @@
 package com.web_services.instant_pot.domain;
 
-public class Address {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Address {
+	@Id
+	@Column (name="address_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String streetAddress;
+	
+	private String city;
+	
+	private String state;
+	
+	private String zip;
 }
--- instant_pot/src/main/java/com/web_services/instant_pot/domain/Payment.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/domain/Payment.java
@@ -1,5 +1,14 @@
 package com.web_services.instant_pot.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Payment {
+	@Id
+	@Column (name="payment_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
 
 }
