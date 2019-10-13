a19f3b7ace08ad2584d9b634f89a3100236dc3a6
Jonathan <warkentin.jon@gmail.com>
2019-10-13 12:03:05 -0500

    fill in remaining getters and setters for Review model

--- instant_pot/src/main/java/com/web_services/instant_pot/domain/Review.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/domain/Review.java
@@ -26,6 +26,30 @@ public class Review implements Serializable {
 	@JoinColumn(name = "fk_customer")
 	private Customer customer;
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
 	private int rating;
 	
 	private String comment;
