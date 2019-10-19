1e95f55d27d188224a45a2fb818410aa15939130
Jonathan <warkentin.jon@gmail.com>
2019-10-18 19:57:44 -0500

    create a bunch more instances of objects in App

--- instant_pot/src/main/java/com/web_services/instant_pot/App.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/App.java
@@ -34,9 +34,22 @@ public class App
     	
     	// Create model instances
     	Customer customer1 = customerLogic.createCustomer("Bob", "Dylan", "bobdylan@luc.edu", 1234567890l);
+    	Customer customer2 = customerLogic.createCustomer("Snoop", "Dogg", "snoopdogg@hotmail.com", 4567891234l);
     	Partner partner1 = partnerLogic.createPartner("McDonalds", "Food", "Sells hamburgers.");
+    	Partner partner2 = partnerLogic.createPartner("Burger King", "Food", "Sells chicken fries.");
     	Payment payment1 = paymentLogic.createPayment(5647382910293847l, 1122, 456);
+    	Payment payment2 = paymentLogic.createPayment(8888999922223333l, 0521, 343);
     	Product product1 = productLogic.createProduct(partner1, "Product 1", "The first product.", 25.00);
+    	Product product2 = productLogic.createProduct(partner1, "Product 2", "The second product.", 20.00);
+    	Product product3 = productLogic.createProduct(partner1, "Product 3", "The third product.", 17.00);
+    	Product product4 = productLogic.createProduct(partner1, "Product 4", "The fourth product.", 230.00);
     	Review review1 = reviewLogic.addReview(customer1.getId(), product1.getId(), 5, "Product 1 is great!", System.currentTimeMillis());
+    	Review review2 = reviewLogic.addReview(customer2.getId(), product1.getId(), 3, "Product 1 is alright!", System.currentTimeMillis());
+    	Review review3 = reviewLogic.addReview(customer1.getId(), product2.getId(), 4, "Product 1 is good!", System.currentTimeMillis());
+    	Review review4 = reviewLogic.addReview(customer2.getId(), product2.getId(), 2, "Product 1 is pretty bad!", System.currentTimeMillis());
+    	Review review5 = reviewLogic.addReview(customer1.getId(), product3.getId(), 5, "Product 1 is great!", System.currentTimeMillis());
+    	Review review6 = reviewLogic.addReview(customer2.getId(), product3.getId(), 1, "Product 1 is awful!", System.currentTimeMillis());
+    	Review review7 = reviewLogic.addReview(customer1.getId(), product4.getId(), 5, "Product 1 is great!", System.currentTimeMillis());
+    	Review review8 = reviewLogic.addReview(customer2.getId(), product4.getId(), 3, "Product 1 is alright!", System.currentTimeMillis());
     }
 }
