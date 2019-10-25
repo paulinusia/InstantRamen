31de516662a466551b0e5b30ff72c9338d694b79
Jonathan <warkentin.jon@gmail.com>
2019-10-25 00:03:25 -0500

    add path, get, consumes, and produces annotations to productresource - server should run now with right environment settings

--- instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductResource.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductResource.java
@@ -2,11 +2,21 @@ package com.web_services.instant_pot.service.product;
 
 import java.util.Set;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 import com.web_services.instant_pot.service.product.representation.ProductRepresentation;
 import com.web_services.instant_pot.service.product.workflow.ProductActivity;
 
+@Path("/productservice/")
 public class ProductResource implements ProductService{
 
+	@GET
+	@Consumes({"application/xml" , "application/json"})
+	@Produces({"application/xml" , "application/json"})
+	@Path("/products")
 	public Set<ProductRepresentation> getProducts() {
 		System.out.println("GET METHOD Request for all products .............");
 		ProductActivity productActivity = new ProductActivity();
