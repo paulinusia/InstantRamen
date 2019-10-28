defcd22787a1c2fcbe7e5ce401b18b66ea8e80d3
Jonathan <warkentin.jon@gmail.com>
2019-10-27 19:29:19 -0500

    add xml annotations to paymentrequest

--- instant_pot/src/main/java/com/web_services/instant_pot/service/payment/representation/PaymentRequest.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/payment/representation/PaymentRequest.java
@@ -1,5 +1,13 @@
 package com.web_services.instant_pot.service.payment.representation;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "Payment")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
 public class PaymentRequest {
 	private String type;
 	
