5a6ea3049258b187443593ff5d1ae83fdd12f7bb
Jonathan <warkentin.jon@gmail.com>
2019-11-03 13:10:27 -0600

    auto gen methods in productresource

--- instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductResource.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import com.web_services.instant_pot.service.product.representation.ProductRepresentation;
 import com.web_services.instant_pot.service.product.workflow.ProductActivity;
@@ -20,6 +21,32 @@ public class ProductResource implements ProductService{
 		System.out.println("GET METHOD Request for all products .............");
 		ProductActivity productActivity = new ProductActivity();
 		return productActivity.getProducts();	
+	}
+
+	public ProductRepresentation getProductById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<ProductRepresentation> searchProducts(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ProductRepresentation createProduct(Long partnerId, String productName, String productDescription,
+			double cost) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Response updateProduct(Long partnerId, String productName, String productDescription, double cost) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Response deleteProduct(Long productId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
