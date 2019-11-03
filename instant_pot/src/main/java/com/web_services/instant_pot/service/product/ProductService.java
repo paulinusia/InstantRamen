4060fa2e2b3782677de46864026b438f66ab8711
Jonathan <warkentin.jon@gmail.com>
2019-11-03 13:09:18 -0600

    add all product methods in productservice

--- instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductService.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/product/ProductService.java
@@ -3,11 +3,23 @@ package com.web_services.instant_pot.service.product;
 import java.util.Set;
 
 import javax.jws.WebService;
+import javax.ws.rs.core.Response;
 
+import com.web_services.instant_pot.domain.partner.Partner;
 import com.web_services.instant_pot.service.product.representation.ProductRepresentation;
 
 
 @WebService
 public interface ProductService {
 	public Set<ProductRepresentation> getProducts();
+	
+	public ProductRepresentation getProductById(Long id);
+	
+	public Set<ProductRepresentation> searchProducts(String name);
+	
+	public ProductRepresentation createProduct(Long partnerId, String productName, String productDescription, double cost);
+	
+	public Response updateProduct(Long partnerId, String productName, String productDescription, double cost);
+	
+	public Response deleteProduct(Long productId);
 }
