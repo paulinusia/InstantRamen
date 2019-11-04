1f1550b2f06af4e10393ce7e6dde38dc3df38b94
Jonathan <warkentin.jon@gmail.com>
2019-11-04 17:10:16 -0600

    add file structure for address service

---
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/address/representation/AddressRequest.java
@@ -0,0 +1,5 @@
+package com.web_services.instant_pot.service.address.representation;
+
+public class AddressRequest {
+
+}
---
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/address/workflow/AddressActivity.java
@@ -0,0 +1,5 @@
+package com.web_services.instant_pot.service.address.workflow;
+
+public class AddressActivity {
+
+}
