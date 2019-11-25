7a1e0012964f41a46539d354e3d14135bf48c8ce
Jonathan <warkentin.jon@gmail.com>
2019-11-24 19:13:33 -0600

    setup Link initalization structure

--- instant_pot/src/main/java/com/web_services/instant_pot/service/customer/workflow/CustomerActivity.java
+++ instant_pot/src/main/java/com/web_services/instant_pot/service/customer/workflow/CustomerActivity.java
@@ -9,13 +9,23 @@ import com.web_services.instant_pot.domain.partner.PartnerLogic;
 import com.web_services.instant_pot.domain.product.Product;
 import com.web_services.instant_pot.service.customer.representation.CustomerRepresentation;
 import com.web_services.instant_pot.service.customer.representation.CustomerRequest;
+import com.web_services.instant_pot.service.link.Link;
 import com.web_services.instant_pot.service.product.representation.ProductRepresentation;
 
 public class CustomerActivity {
 	CustomerLogic customerLogic = new CustomerLogic();
 	
 	public CustomerRepresentation getCustomerById(Long id) {
-		return getCustomerRepresentation(customerLogic.getCustomerById(id));
+		CustomerRepresentation cRep = getCustomerRepresentation(customerLogic.getCustomerById(id));
+		Set<Link> links = new HashSet<>();
+		// update customer; require media type application/json or application/xml
+		// delete customer
+		// create address
+		// create purchase
+		// get all addresses for customer
+		// get all purchases for customer
+		cRep.setLinks(links);
+		return cRep;
 	}
 	
 	public Set<CustomerRepresentation> getCustomers() {
@@ -43,13 +53,30 @@ public class CustomerActivity {
 	public CustomerRepresentation createCustomer(CustomerRequest customerRequest) {
 		CustomerLogic customerLogic = new CustomerLogic();
 		Customer newCustomer =  customerLogic.createCustomer(customerRequest.getFirstName(), customerRequest.getLastName(), customerRequest.getEmail(), customerRequest.getPhoneNumber());
-		return getCustomerRepresentation(newCustomer);
+		CustomerRepresentation cRep = getCustomerRepresentation(newCustomer);
+		Set<Link> links = new HashSet<>();
+		// update customer; require media type application/json or application/xml
+		// delete customer
+		// create address
+		// create purchase
+		// get purchases for customer
+		// get addresses for customer
+		cRep.setLinks(links);
+		return cRep;
 	}
 
 	public CustomerRepresentation updateCustomer(Long id, CustomerRequest customerRequest) {
 		CustomerLogic customerLogic = new CustomerLogic();
 		Customer customer = customerLogic.updateCustomer(id, customerRequest.getFirstName(), customerRequest.getLastName(), customerRequest.getEmail(), customerRequest.getPhoneNumber());
-		return getCustomerRepresentation(customer);
+		CustomerRepresentation cRep = getCustomerRepresentation(customer);
+		Set<Link> links = new HashSet<>();
+		// delete customer
+		// create address
+		// create purchase
+		// get purchases for customer
+		// get addresses for customer
+		cRep.setLinks(links);
+		return cRep;
 	}
 
 	public static CustomerRepresentation deleteCustomer(Long id) {
